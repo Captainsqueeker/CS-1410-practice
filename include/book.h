33ecf12b73fb5f512c6a50025c1b17369ddfdfaa
Captainsqueeker <44932785+Captainsqueeker@users.noreply.github.com>
2025-10-28 05:40:46 +0000

    Finished ex0802

---
+++ include/book.h
@@ -0,0 +1,33 @@
+#ifndef __BOOK_H__
+#define __BOOK_H__
+
+#include <iostream>
+#include <string>
+#include "author.h"
+
+
+class Book {
+private:
+    std::string title;
+    int year;
+    Author author;
+public:
+    Book(const std::string& t, int y, Author& a);
+
+    std::string getTitle() const;
+    int getYear() const;
+    Author getAuthor() const;
+
+    void setTitle(const std::string& t);
+    void setYear(int y);
+    void setAuthor(const Author& a);
+
+    void show() const{
+        std::cout << "Book: " << title << "( " << year << " )" << endl;
+        std::cout << "Author: " << author.getName() << "[" << author.getNationality() << "]" << endl;
+
+    }
+};
+
+
+#endif
---
+++ src/book.cpp
@@ -0,0 +1,12 @@
+#include "book.h"
+#include "author.h"
+
+    Book::Book(const std::string& t, int y, Author& a) : title(t), year(y), author(a){}
+
+    std::string Book::getTitle() const{return title;}
+    int Book::getYear() const{return year;}
+    Author Book::getAuthor() const{return author;}
+
+    void Book::setTitle(const std::string& t){title = t;}
+    void Book::setYear(int y){year = y;}
+    void Book::setAuthor(const Author& a){author = a;}
